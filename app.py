0fce2d03d3ac9540378f3db493a3220feb0bf3b6
archiearora21 <103375760+archiearora21@users.noreply.github.com>
2022-04-11 12:12:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,75 +1,27 @@
-import os
-from flask import Flask, render_template
-from flask import request
+!pip3 install ktrain
+import os.path
+import numpy as np
+import tensorflow as tf
+import ktrain
+from ktrain import text
 
+dataset = tf.keras.utils.get_file(fname="aclImdb_v1.tar.gz",origin="http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz",extract=True)
+IMDB_DATADIR = os.path.join(os.path.dirname(dataset), 'aclImdb')
+print(os.path.dirname(dataset))
+print(IMDB_DATADIR)
 
-import torch
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+(x_train, y_train), (x_test, y_test), preproc = text.texts_from_folder(datadir=IMDB_DATADIR,
+                       classes=['pos','neg'],
+                       maxlen=500,
+                       train_test_names=['train','test'],
+                       preprocess_mode='bert')
 
-name = "mrm8488/bert-small-finetuned-squadv2"
+model = text.text_classifier(name='bert',
+                             train_data=(x_train, y_train),  
+                             preproc=preproc)
 
-tokenizer = AutoTokenizer.from_pretrained(name,)
-
-model = AutoModelForQuestionAnswering.from_pretrained(name)
-
-def answer_question(question, answer_text):
-    '''
-    Takes a `question` string and an `answer` string and tries to identify 
-    the words within the `answer` that can answer the question. Prints them out.
-    '''
-    
-    # tokenize the input text and get the corresponding indices
-    token_indices = tokenizer.encode(question, answer_text)
-
-    # Search the input_indices for the first instance of the `[SEP]` token.
-    sep_index = token_indices.index(tokenizer.sep_token_id)
-
-    seg_one = sep_index + 1
-
-    # The remainders lie in the second segment.
-    seg_two = len(token_indices) - seg_one
-    
-    # Construct the list of 0s and 1s.
-    segment_ids = [0]*seg_one + [1]*seg_two
-
-    # get the answer for the question
-    start_scores, end_scores = model(torch.tensor([token_indices]), # The tokens representing our input combining question and answer.
-                                    token_type_ids=torch.tensor([segment_ids])) # The segment IDs to differentiate question from answer
-
-    # Find the tokens with the highest `start` and `end` scores.
-    answer_begin = torch.argmax(start_scores)
-    answer_end = torch.argmax(end_scores)
-
-    # Get the string versions of the input tokens.
-    indices_tokens = tokenizer.convert_ids_to_tokens(token_indices)
-    
-    answer = indices_tokens[answer_begin:answer_end+1]
-    #remove special tokens
-    answer = [word.replace("▁","") if word.startswith("▁") else word for word in answer] #use this when using model "twmkn9/albert-base-v2-squad2"
-    answer = " ".join(answer).replace("[CLS]","").replace("[SEP]","").replace(" ##","")
-    
-    return answer
-
-
-app = Flask(__name__)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-  
-    if request.method == 'POST':
-      form = request.form
-      result = []
-      bert_abstract = form['paragraph']
-      question = form['question']
-      result.append(form['question'])
-      result.append(answer_question(question, bert_abstract))
-      result.append(form['paragraph'])
-
-      return render_template("index.html",result = result)
-
-    return render_template("index.html")
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+learner = ktrain.get_learner(model=model,
+                             train_data=(x_train,y_train),
+                             val_data=(x_test, y_test),batch_size=6)
+                             learner.fit_onecycle(lr=2e-5,epochs=1)
+learner.fit_onecycle(lr=2e-5,epochs=1)
